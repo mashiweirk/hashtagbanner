41d0a0717717a95868bc076b09baada2981e8046
Mark Witham <mwitham@basicincomeproject.org>
2015-08-07 18:41:01 -0400

    Donate and Text
    Donate Section
    Changed text Wording

--- wp-banner-plugin/js/fbscripts.js
+++ wp-banner-plugin/js/fbscripts.js
@@ -61,13 +61,13 @@ function uploadPhotoToFacebook() {
     
     // Create donate button
     var node = document.getElementById("donate");
-    var htmlp = '<p>We need your help in spreading this campaign. Supporting the Basic Income Hashtag Banner Campaign by donating will help us to continue to make improvements and upkeep.</p><br/>' +
-    '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">' +
+    var htmlp = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">' +
     '<input type="hidden" name="cmd" value="_s-xclick">' +
     '<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHXwYJKoZIhvcNAQcEoIIHUDCCB0wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC8MPekozh4XukBAW2MsticlIJ7f5423JLkTqw02hVwOVaIpanqrPyaJUbnFsSwe1dlGjnA0Wi/3Cvp7DSxBkWDe0F1SCVFwRq4k/G5uzuKGTeFnHztJ/d6lm+VMwKjisxERdsIVUxmRXgHB+YFNanCSJfA7x295W54k7otzhL8DTELMAkGBSsOAwIaBQAwgdwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIFFlbigMMGymAgbhrr2jVPPV1RVj638aCaTB5ssUFDv+NzBogieI9oUnBn8fozlMVNpxOXR9khjJCANQhmt7Te6uOJmU+RXGO0z8yFHBsQjWg60ZLZS8PUnmv+lIqxScl9qGKQ+yC8rGH3v190PoLgT6XfO6tCqtTmGcbDVtnFFgMhitig96j8tO/WanGdw+INhov9eUU7Lmr6xIe4IupsNIpyW+i/2unvSVklO04wBJdavj5oUBs/WutshtgT0s321CMoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUwODA1MDEzNjQ4WjAjBgkqhkiG9w0BCQQxFgQUPVvxd3fXJT24uABkbe0QXKZPjeEwDQYJKoZIhvcNAQEBBQAEgYBbqMr8qeSqiS5DehxZrzICuzpeLS7grV2CU3TSGXV9AI1RgHvyGzIw35tVVeoYjSmgruPY00j9VMLJUJRUCQCa2Hr3G/VxXHRV7wGTY4SSlzmEKHULM6QWsryjZT8mRWb+x40SIxy7gPnV8vFLuza0uyvRGIgvGtF3zrGR0yFOUw==-----END PKCS7-----">' +
     '<input type="submit" value="Donate" name="submit" title="PayPal - The safer, easier way to pay online!" class="btn btn-lg btn-donate">' +
     '<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">' +
-    '</form>';
+    '</form>' +
+    '<p>We need your help building a bigger campaign for improving this and promting more ideas to help spread the idea of basic income!</p><br/>';
     
     node.innerHTML = htmlp;
     
@@ -101,6 +101,7 @@ function uploadPhotoToFacebook() {
             success:function(data){
                 console.log("success " + data);
                 //TODO: open this as a popup window... on desktops, this will create an ADDITIONAL popup that offers to set the photo as the profile photo
+                //TODO: switched to mobile FB to avoid croping , need device browser recognition or no cropping.
                 window.open("http://m.facebook.com/photo.php?fbid=" + data.id + "&makeprofile=1", "_blank");
             },
             error:function(shr,status,data){
@@ -113,7 +114,7 @@ function uploadPhotoToFacebook() {
 
     } catch(e){
         console.log(e);
-    }    	
+    }
 }
 
 function getProfileImage() {
